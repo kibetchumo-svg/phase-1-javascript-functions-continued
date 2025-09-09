3c5f23e9ef606ab0adb3df44999322808cc20160
kibetchumo-svg <kibet.chumo@student.moringaschool.com>
2025-09-09 21:33:14 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+/* 1*/
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun()); 
+console.log(saturdayFun("bathe my dog")); 
+
+/* 2*/
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+console.log(mondayWork()); 
+console.log(mondayWork("work from home")); 
+
+/* 3 */
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+const encourage = wrapAdjective("!!!");
+console.log(encourage("a dedicated programmer"));
+
+console.log(wrapAdjective("%")("awesome"));
+
